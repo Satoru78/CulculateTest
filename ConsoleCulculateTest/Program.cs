b535ba6dd84f2963b29f229e9bf67384c7987ad8
Satoru78 <viewgold931@gmail.com>
2022-02-21 22:31:36 +0300

    Добавьте файлы проекта.

---
+++ ConsoleCulculateTest/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleCulculateTest;
+using CulculateTest;
+
+namespace ConsoleCulculateTest
+{
+    class Program
+    {
+        private static int Action { get; set; }
+        static void Main()
+        {
+            while (Action !=5)
+            {
+                Commands();
+                Console.WriteLine("Введите команду: ");           
+                Action = int.Parse(Console.ReadLine());
+                switch (Action)
+                {
+                    case 1:
+                     {
+                        Console.WriteLine("Ввведите первое число:");
+                        int firstnum = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Ввведите первое число:");
+                        int secondnum = int.Parse(Console.ReadLine());
+                        Console.WriteLine(Culculate.Addition(firstnum, secondnum));
+                     }
+                        break;
+                    case 2:
+                     {
+                            Console.WriteLine("Ввведите первое число:");
+                            int firstnum = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Ввведите первое число:");
+                            int secondnum = int.Parse(Console.ReadLine());
+                            Console.WriteLine(Culculate.Substraction(firstnum, secondnum));
+                     }
+                        break;
+                    case 3:
+                        {
+                            Console.WriteLine("Ввведите первое число:");
+                            int firstnum = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Ввведите первое число:");
+                            int secondnum = int.Parse(Console.ReadLine());
+                            Console.WriteLine(Culculate.Multiplicaton(firstnum, secondnum));
+                        }
+                        break;
+                    case 4:
+                        {
+                            Console.WriteLine("Ввведите первое число:");
+                            int firstnum = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Ввведите первое число:");
+                            int secondnum = int.Parse(Console.ReadLine());
+                            Console.WriteLine(Culculate.Division(firstnum, secondnum));
+                        }
+                        break;
+                    case 5:
+                        Process.GetCurrentProcess().Kill();
+                        break;
+
+                }  
+            }
+           
+        }
+
+        static void Commands()
+        {
+           Console.WriteLine("1.Сложение");
+           Console.WriteLine("2.Вычитание");
+           Console.WriteLine("3.Умножение");
+           Console.WriteLine("4.Деление");
+           Console.WriteLine("5.Закрыть программу");
+
+        }
+    }
+}
---
+++ CulculateTest/Culculate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CulculateTest
+{
+    public class Culculate
+    {
+        public static int Addition(int firstnum , int secondnum)
+        {
+            return firstnum + secondnum;
+        }
+        public static int Substraction(int firstnum, int secondnum)
+        {
+            return firstnum - secondnum;
+        }
+        public static int Multiplicaton(int firstnum , int secondnum)
+        {
+            return firstnum * secondnum;
+        }
+        public static int Division(int firstnum , int secondnum)
+        {
+            if(secondnum == 0)
+            {
+                return  0;
+            }
+            else
+            {
+               return firstnum / secondnum;
+            }
+        }
+    }
+}
